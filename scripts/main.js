37e4eca987e61cdaac7337385ce25d6e9ec336de
Matcha2Smiley <greentea2Smiley@gmail.com>
2024-02-05 23:08:40 +0900

    メニュー関数, 計算式登録プッシュ用関数,計算式用フォーム生成関数,計算式選択時プッシュ用関数の追加

--- scripts/main.js
+++ scripts/main.js
@@ -11,12 +11,12 @@ function init(){
     button = document.getElementById('button');
     hidden = document.getElementById('hidden');
 
-    createDefaultFotm();
-    CSSLayerStatementRule();
+    createDefaultForm();
+    createMenu();
 }
 
 //計算式登録用フォームの作成
-function createDefaultFotm(){
+function createDefaultForm(){
     var tr, th, td, input;
     var names = ['name', 'fields', 'formula'];
     form.innerHTML = '';
@@ -44,10 +44,123 @@ function createDefaultFotm(){
 
     //hidden, msgの値の設定
     hidden.value = '';
-    msg.textContent = '新しい計算式の入力'
+    msg.textContent = '新しい計算式の入力';
     //onclickイベントの関数を削除
     button.removeEventListner('click', doCalcAction);
     button.removeEventListner('click', doDefaultAction);
-    //buttonのonclickイベントにdoDefaultActionを設定
+    //buttonのonclickイベントにdoDefaultActionを設定button.
     button.addEventListener('click', doDefaultAction, false);
+}
+
+//メニューの作成
+function createMenu(){
+    //menuをクリア
+    menu.innerHTML = '';
+    //新しい式を作成メニューを用意
+    var opt = document.createElement("option");
+    opt.id = '0';
+    opt.value = 'default';
+    menu.appendChild(opt);
+    //登録済みの関数の個数を取り出す
+    var count = localStorage.getItem('count')*1;
+    //繰り返しでメニュー項目を作成していく
+    for(var i = 1; i <= count; i++){
+        //<option>タグを作成
+        opt = document.createElement("option");
+        opt.id = i;
+        //ローカルストレージからオブジェクトを取得
+        var opt_val = localStorage.getItem('formula_'+i);
+        var opt_obj = JSON.parse(opt_val);
+        //必要な値を<option>に設定し組み込む
+        opt.textContent = opt_obj.name;
+        opt.value = 'formula_' + i;
+        menu.appendChild(opt);
+    }
+}
+
+//計算式登録用のプッシュボタン用関数
+function doDefaultAction(){
+    //登録済みの関数の個数を取り出す
+    var count = localStorage.getItem('count')*1;
+    //オブジェクトを作成し、プロパティを設定
+    var obj = new Object();
+    obj.name = document.getElementById('name').value;
+    obj.fields = document.getElementById('fields').value;
+    obj.formula = document.getElementById('formula').value;
+    //オブジェクトをローカルストレージに保存
+    localStorage.setItem('formula_'+ ++count, JSON.stringify(obj));
+    //登録済みの関数の個数を更新
+    localStorage.setItem('count',count);
+    //フォームとメニューを生成する
+    createDefaultForm();
+    createMenu();
+    alert('追加しました。');
+}
+
+//メニューから選んだ計算式用のフォーム生成
+function doChangeForm(event){
+    //選んだメニュー項目のvalueを取得
+    var value = event.target.value;
+    //valueがdefaultだったらcreateDefaultForm実行
+    if(value=='defalut'){
+        createDefaultForm();
+        return;
+    }
+
+    //ローカルストレージから値を取得
+    var obj_str = localStorage.getItem(value);
+    //オブジェクトに変換し、必要な値を用意する。
+    var obj = JSON.parse(obj_str);
+    var fields = obj.fields.split(',');
+    var formula = obj.formula;
+
+    var tr, th, td, label, input;
+    form.innerHTML = '';
+    //fields配列を元にフィールドを生成
+    for(var i = 0;i < fields.length;i++){
+        //必要なタグのDOMを作成
+        tr = document.createElement("tr");
+        th = document.createElement("th");
+        th.textContent = fields[i];
+        td = document.createElement("input");
+        //<input>タグを生成
+        input = document.createElement("input");
+        input.type = 'text';
+        input.id = fields[i];
+        //作成したタグを組み込む
+        td.appendChild(input);
+        tr.appendChild(th);
+        tr.appendChild(td);
+        forom.appendChild(tr);
+    } 
+
+    //hidden, msgの値設定
+    hidden.value = value;
+    msg.textContent = obj.name;
+    //onclickイベントの関数を削除
+    button.removeEventListner('click', doCalcAction);
+    button.removeEventListner('click', doDefaultAction);
+    //buttonのonclickイベントにdoCalcActionを設定
+    button.add.addEventListener('click', doCalcAction, false);
+}
+
+//計算式選択時のプッシュボタン用関数
+function doCalcAction(){
+    //hiddenの値を元にオブジェクトを用意
+    var obj_str = localStorage.getItem(hidden.value);
+    var obj = JSON.parse(obj_str);
+    //names, count, formulaを用意
+    var names = obj.fields.split(',');
+    var count = names.length;
+    var formula = obj.formula;
+    //formulaの値名を実際の値に置換する
+    for(var i = 0;i < count; i++){
+        var name = names[i];
+        var value = document.getElementById(name).value;
+        formula = formula.split(name).join(value);
+    }
+
+    //formulaの式を実行し結果を表示
+    var result = eval(formula);
+    msg.textContent = 'result:' + result;
 }
